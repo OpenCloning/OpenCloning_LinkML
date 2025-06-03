e304ea215c8475622b45c506abee420e71cda644
Manuel Lera-Ramirez <manulera14@gmail.com>
2025-06-03 15:14:50 +0100

    add support for zero-length locations in the converter (#54)

--- src/opencloning_linkml/migrations/transformations/v0_2_8_to_v0_2_9.py
+++ src/opencloning_linkml/migrations/transformations/v0_2_8_to_v0_2_9.py
@@ -12,7 +12,10 @@ def migrate_0_2_8_to_0_2_9(data: dict) -> dict:
                 for side in ["left_location", "right_location"]:
                     if assembly_fragment[side] is not None:
                         location = assembly_fragment[side]
-                        assembly_fragment[side] = f"{location['start'] + 1}..{location['end']}"
+                        if location["start"] != location["end"]:
+                            assembly_fragment[side] = f"{location['start'] + 1}..{location['end']}"
+                        else:
+                            assembly_fragment[side] = f"{location['start']}^{location['start'] + 1}"
         elif source["type"] == "UploadedFileSource" and "coordinates" in source:
             if source["coordinates"] is not None:
                 source["coordinates"] = f"{source['coordinates']['start'] + 1}..{source['coordinates']['end']}"
